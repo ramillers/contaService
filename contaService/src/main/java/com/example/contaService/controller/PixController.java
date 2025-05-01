5417a1d11550ea945caab061e6ca953c5de8b518
ramillers <rmillesantana@gmail.com>
2025-05-01 11:41:22 -0300

    feat(controller): add PixController for handling Pix operations

---
+++ contaService/src/main/java/com/example/contaService/controller/PixController.java
@@ -0,0 +1,29 @@
+package com.example.contaService.controller;
+
+
+import com.example.contaService.dto.PixRequestDTO;
+import com.example.contaService.dto.PixResponseDTO;
+import com.example.contaService.service.PixService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/pix")
+@RestController
+@RequiredArgsConstructor
+public class PixController {
+
+    private final PixService pixService;
+
+    @PostMapping
+    public ResponseEntity<PixResponseDTO> pix(@RequestBody @Valid PixRequestDTO pixRequestDTO) {
+        PixResponseDTO pixResponseDTO = pixService.realizaPix(pixRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pixResponseDTO);
+    }
+
+}
