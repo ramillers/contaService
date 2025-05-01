3b807c5a258ebbf73f8a35a0dedbd174d589129e
ramillers <rmillesantana@gmail.com>
2025-05-01 11:56:04 -0300

    feat(feign-dto): add DTOs for Pix key operations with Bacen

---
+++ contaService/src/main/java/com/example/contaService/feign/dto/ChaveRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.contaService.feign.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChaveRequestDTO {
+    private String chave;
+    private Boolean ativa;
+}
