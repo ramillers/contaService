9be226170ccd5de0efea602c9127e34e4e6719eb
ramillers <rmillesantana@gmail.com>
2025-05-01 11:55:09 -0300

    feat(exception): add custom exceptions for Pix and Bacen integration

---
+++ contaService/src/main/java/com/example/contaService/exception/ErroIntegracaoBacenException.java
@@ -0,0 +1,7 @@
+package com.example.contaService.exception;
+
+public class ErroIntegracaoBacenException extends RuntimeException {
+    public ErroIntegracaoBacenException(String message) {
+        super(message);
+    }
+}
---
+++ contaService/src/main/java/com/example/contaService/exception/PixNegativoException.java
@@ -0,0 +1,7 @@
+package com.example.contaService.exception;
+
+public class PixNegativoException extends RuntimeException {
+    public PixNegativoException(String message) {
+        super(message);
+    }
+}
