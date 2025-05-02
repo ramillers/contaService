0e6fe4f155220d1994df402fe0a27fbdf2e01e09
ramillers <rmillesantana@gmail.com>
2025-05-02 15:16:16 -0300

    feat(repository): add PixRepository for managing database operations

---
+++ contaService/src/main/java/com/example/contaService/repository/PixRepository.java
@@ -0,0 +1,12 @@
+package com.example.contaService.repository;
+
+import com.example.contaService.model.Pix;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PixRepository extends JpaRepository<Pix, UUID> {
+    Optional<Pix> findByIdempotencia(final String idempotencia);
+}
